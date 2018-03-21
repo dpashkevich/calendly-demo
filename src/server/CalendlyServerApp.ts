162eb24d08b7cfd32f26cb4d147ebb7ec2d4064e
dpashkevich <dpashk@gmail.com>
2018-03-21 00:53:51 -0600

    Store recent events in memory and retrieve on the client

--- src/server/CalendlyServerApp.ts
+++ src/server/CalendlyServerApp.ts
@@ -8,6 +8,8 @@ import * as io from 'socket.io';
  * Server-side app to manage Calendly events
  */
 export class CalendlyServerApp {
+  private recentEvents: object[] = [];
+  private MAX_RECENT_EVENTS = 10;
 
   constructor(private port: number) {}
 
@@ -26,7 +28,16 @@ export class CalendlyServerApp {
     app.use(express.static(path.join(__dirname, '../../public')));
     app.use(bodyParser.json());
 
+    app.get('/recentEvents', (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(this.recentEvents));
+    });
+
     app.post('/webhook', (req, res) => {
+      this.recentEvents.push(req.body);
+      if (this.recentEvents.length > this.MAX_RECENT_EVENTS) {
+        this.recentEvents.shift();
+      }
       socketIo.emit('new event', JSON.stringify(req.body));
       res.sendStatus(201);
     });
