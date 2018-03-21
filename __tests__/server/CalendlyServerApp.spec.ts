c79d3f6ba3b98bb9f480da1476d9890bf0827c7d
dpashkevich <dpashk@gmail.com>
2018-03-21 01:33:09 -0600

    Add more tests

--- __tests__/server/CalendlyServerApp.spec.ts
+++ __tests__/server/CalendlyServerApp.spec.ts
@@ -25,4 +25,93 @@ describe('Calendly Server App', () => {
       .send({})
       .expect(201, done);
   });
+
+  it('does not respond to GET /webhook', (done) => {
+    request(server)
+      .get('/webhook')
+      .expect(404, done);
+  });
+
+  it('does not respond to /non-existent', (done) => {
+    request(server)
+      .get('/non-existent')
+      .expect(404, done);
+  });
+
+  it('initially an empty array of recent events', (done) => {
+    request(server)
+      .get('/recentEvents')
+      .expect('Content-Type', /json/)
+      .expect(200, [], done);
+  });
+
+  it('returns recent events', (done) => {
+    const events = [
+      {event: {uuid: 1}},
+      {event: {uuid: 2}},
+      {event: {uuid: 3}},
+    ];
+    /* tslint:disable:no-any */
+    let currentPromise: Promise<any> = Promise.resolve();
+
+    // run the async requests in sequence
+    events.forEach((e) => {
+      currentPromise = currentPromise.then(() => {
+        return request(server)
+          .post('/webhook')
+          .set('Content-Type', 'application/json')
+          .send(e)
+          .expect(201);
+      });
+    });
+
+    /* tslint:disable:no-floating-promises */
+    currentPromise.then(() => {
+      request(server)
+      .get('/recentEvents')
+      .expect('Content-Type', /json/)
+      .expect(200, events, done);
+    });
+  });
+
+  it('returns only 10 recent events', (done) => {
+    const events = [
+      {event: {uuid: 1}},
+      {event: {uuid: 2}},
+      {event: {uuid: 3}},
+      {event: {uuid: 4}},
+      {event: {uuid: 5}},
+      {event: {uuid: 6}},
+      {event: {uuid: 7}},
+      {event: {uuid: 8}},
+      {event: {uuid: 9}},
+      {event: {uuid: 10}},
+      {event: {uuid: 11}},
+      {event: {uuid: 12}},
+    ];
+    /* tslint:disable:no-any */
+    let currentPromise: Promise<any> = Promise.resolve();
+
+    // run the async requests in sequence
+    events.forEach((e) => {
+      currentPromise = currentPromise.then(() => {
+        return request(server)
+          .post('/webhook')
+          .set('Content-Type', 'application/json')
+          .send(e)
+          .expect(201);
+      });
+    });
+
+    const recentEvents = events.slice();
+    recentEvents.splice(0, 2);
+
+    /* tslint:disable:no-floating-promises */
+    currentPromise.then(() => {
+      request(server)
+      .get('/recentEvents')
+      .expect('Content-Type', /json/)
+      .expect(200, recentEvents, done);
+    });
+  });
 });
